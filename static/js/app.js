270e2a3a7d4f350eec27868516c389fe939d43b8
Thomas Nakamoto <tnakamoto@me.com>
2018-11-05 17:40:10 -0800

    working date filter

--- static/js/app.js
+++ static/js/app.js
@@ -1,48 +1,68 @@
 // from data.js
-let tableData = data;
+const tableData = data;
 
 // YOUR CODE HERE!
 
-let tbody = d3.select("tbody");
+const tbody = d3.select("tbody");
 
 
-tableData.forEach((entry) => {
-  let trow = tbody.append("tr");
+function fillTable(subTable) {
+  subTable.forEach((entry) => {
+  const appRow = tbody.append("tr");
   Object.entries(entry).forEach(([key, value]) => {
-    let tcell = trow.append("td");
-    tcell.text(value);
+    const appCell = appRow.append("td");
+    appCell.text(value);
   });
-});
+})};
 
 // filtering data down
-let dateInput = d3.select("#datetime");
-let dateButton = d3.select("#filter-btn");
+const dateInput = d3.select("#datetime");
+const cityInput = d3.select("#city");
+const stateInput = d3.select("#state");
+const countryInput = d3.select("#country");
+const shapeInput = d3.select("#shape");
+const filterButton = d3.select("#filter-btn");
 
-dateButton.on("click", function() {
+filterButton.on("click", function() {
   d3.event.preventDefault();
   d3.selectAll("td").remove();
-  let dateInputValue = dateInput.property("value");
-  console.log(dateInputValue);
-  let filteredDate = tableData.filter(dates => dates.datetime === dateInputValue);
-  console.log(filteredDate);
+
   
-  if (dateInputValue === "") {
-    tableData.forEach((entry) => {
-      let trow = tbody.append("tr");
-      Object.entries(entry).forEach(([key, value]) => {
-        let tcell = trow.append("td");
-        tcell.text(value);
-      });
-    });
+
+  let dateInputValue = dateInput.property("value");
+  let cityInputValue = cityInput.property("value");
+  let stateInputValue = stateInput.property("value");
+  let countryInputValue = countryInput.property("value");
+  let shapeInputValue = shapeInput.property("value");
+  
+
+  let filterObject = {
+    dateKey: dateInputValue,
+    cityKey: cityInputValue,
+    stateKey: stateInputValue,
+    countryKey: countryInputValue,
+    shapeKey: shapeInputValue
+  };
+
+  if (dateInputValue !== "") {
+    tableData.filter(dates => dates.datetime === dateInputValue);
   }
 
-  filteredDate.forEach((entry) => {
-    let trow = tbody.append("tr");
-    Object.entries(entry).forEach(([key, value]) => {
-      let tcell = trow.append("td");
-      tcell.text(value);
-    });
-  });
+  let filteredDate = tableData.filter(dates => dates.datetime === dateInputValue);
+
+  
+  if (dateInputValue === "") {
+    fillTable(tableData);
+  }
+
+  else fillTable(filteredDate);
+
+  console.log(filterObject);
+  console.log(Object.values(filterObject));
+
+
 });
+
+fillTable(tableData);
 
 
